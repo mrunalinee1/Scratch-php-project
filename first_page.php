02d321b6aa2e2a852db0daa0efe624a165de8b24
mrunalinee1 <mrunalineekhairnar@gmail.com>
2024-08-11 14:33:24 +0530

    second

--- first_page.php
+++ first_page.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+<link rel="stylesheet" href="style.css">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -10,14 +11,14 @@
     // Check if form was submitted
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
         // Collect value from the form
-        $name = htmlspecialchars($_POST['name']);
-        echo "<h2>Hello, $name!</h2>";
+        $Name = htmlspecialchars($_POST['name']);
+        echo "<h2>Hello, $Name!</h2>";
     }
     ?>
      <!-- HTML Form -->
      <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
         <label for="name">Enter your name:</label>
-        <input type="text" id="name" name="name" required>
+        <input type="text" name="name" required>
         <input type="submit" value="Submit">
     </form>
 </body>
---
+++ firstpage.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<link rel="stylesheet" href="styles.css">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>First page</title>
+</head>
+<body>
+    <?php
+    echo "hi";
+    ?>
+
+    <label for="Name">Enter your name:</label>
+     <input type="text" id="Name" name="name" required>
+     <input type="submit" value="Submit">
+    
+
+</body>
+</html>
